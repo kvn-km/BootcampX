8255fd5c3eab93c6dda55483b9ab6e09b07da483
Kevin Kim <kevinfkim@gmail.com>
2020-06-25 15:57:03 +0000

    updated teachers.js and students.js to use parameterized queries to escape inputs

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,28 +7,23 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-// pool.query(`
-// SELECT id, name, cohort_id
-// FROM students
-// LIMIT 5;
-// `)
-//   .then(res => {
-//     res.rows.forEach(user => {
-//       console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_id} cohort`);
-//     });
-//   });
-
 const cohortAndNumber = process.argv.slice(2);
+const values = [`%${cohortAndNumber[0]}%`, `${cohortAndNumber[1]}`];
 
-pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '${cohortAndNumber[0].slice(3)}%'
-LIMIT ${cohortAndNumber[1]}
-`)
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+
+pool.query(queryString, values)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
     });
   });
+
+  // RUN WITH
+  // node students.js FEB 2
