9005865f511efc3d6624467b1adb7a6997404ac0
Kevin Kim <kevinfkim@gmail.com>
2020-06-25 06:58:21 +0000

    node-pg queries practice

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,29 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+const cohortName = process.argv[2];
+
+pool.query(`
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = '${cohortName}'
+ORDER BY teacher
+`)
+  .then(res => {
+    res.rows.forEach(teacher => {
+      console.log(`${teacher.cohort} : ${teacher.teacher}`);
+    });
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
